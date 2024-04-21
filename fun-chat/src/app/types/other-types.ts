9ea6859ad9627f5ab62a0a4fb7b4aa2f211aec44
LinderJK <slepchenkovia@gmail.com>
2024-04-21 20:26:13 +0300

    refactor: rename type

--- fun-chat/src/app/types/other-types.ts
+++ fun-chat/src/app/types/other-types.ts
@@ -2,6 +2,6 @@ import { ResponseData } from './response-type';
 
 export type CallbackDataFn = (data: ResponseData) => void;
 
-export type UserData = { login: string; password: string };
+export type UserLoginData = { login: string; password: string };
 
 export type CallbackFn = () => void;
