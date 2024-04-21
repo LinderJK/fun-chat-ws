bd5fd5546c2857510a57a611adb88e7f1e159ac9
LinderJK <slepchenkovia@gmail.com>
2024-04-21 20:25:58 +0300

    feat: change user class, add view, logout method, add getters/setters, random icon color

---
+++ fun-chat/src/app/components/user/User.ts
@@ -0,0 +1,77 @@
+import './user.scss';
+import { UserData } from '../../types/response-type';
+import { div, divText, p } from '../../page/components/BaseComponents';
+import { IComponent } from '../../types/components-types';
+import Network from '../../services/Network';
+
+class User {
+    private readonly privateName: string;
+
+    private readonly privatelogin: string;
+
+    isLogined: boolean;
+
+    view: IComponent | undefined = undefined;
+
+    iconColor: string;
+
+    private readonly password: string = '';
+
+    constructor(data: UserData, password: string = '') {
+        this.privatelogin = data.login;
+        this.privateName = data.login;
+        this.isLogined = data.isLogined;
+        this.iconColor = this.randomColor();
+        this.password = password;
+    }
+
+    render() {
+        const icon = div(
+            'flex-shrink-0 me-2 user-item__icon',
+            p('user-item__icon-text', `${this.shortName()}`)
+        );
+        icon.setAttributes({
+            style: `background-color: ${this.iconColor};`,
+        });
+
+        this.view = div(
+            'd-flex text-muted pt-3 user-item',
+            icon,
+            divText('pb-3 mb-0 small lh-sm w-100', `${this.privateName}`)
+        );
+        return this.view;
+    }
+
+    logout() {
+        console.log('logout');
+        const payloadObj = {
+            user: {
+                login: this.login,
+                password: this.password,
+            },
+        };
+        Network.send({ id: '', type: 'USER_LOGOUT', payload: payloadObj });
+    }
+
+    shortName() {
+        return this.privateName[0];
+    }
+
+    get status() {
+        return this.isLogined;
+    }
+
+    get name() {
+        return this.privateName;
+    }
+
+    get login() {
+        return this.privatelogin;
+    }
+
+    randomColor(): string {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+}
+
+export default User;
