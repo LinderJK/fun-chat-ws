97c32e8e590fbf50df54e6951f5513c44f675387
LinderJK <slepchenkovia@gmail.com>
2025-03-01 16:36:16 +0300

    feat: add deploy

--- cd7716048ffbda0235e6.ts
+++ cd7716048ffbda0235e6.ts
@@ -2,6 +2,5 @@ import './index.html';
 import 'normalize.css';
 import './stylesheets/main.scss';
 import App from './app/app';
-
 const app = new App();
 app.start();
--- fun-chat/src/app/types/other-types.ts
+++
@@ -1,7 +0,0 @@
-import { ResponseData } from './response-type';
-
-export type CallbackDataFn = (data: ResponseData) => void;
-
-export type CallbackFn = () => void;
-
-export type HandlerFn = (evt: Event) => void;
